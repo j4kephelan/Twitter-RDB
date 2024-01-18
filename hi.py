a817b89508c7c07626bead0b84693b5e3eb47032
j4kephelan <jakephelan25@gmail.com>
2024-01-17 23:07:37 -0500

    Stored procedure for inserting tweets working

--- hi.py
+++ hi.py
@@ -8,16 +8,19 @@ password = 'tweet'
 engine = sal.create_engine(f'mysql+pymysql://{username}:{password}@localhost/twitter_rdb')
 
 follows_samples = pd.read_csv('data/follows_sample.csv')
-tweet_samples: pd.DataFrame = pd.read_csv('data/tweets_sample.csv')
+tweet_samples: pd.DataFrame = pd.read_csv('data/tweet.csv')
 
 def insert_tweet(row: pd.Series):
     user_id: int = row['USER_ID']
     tweet_text: str = row['TWEET_TEXT']
     with engine.connect() as conn:
         executable = text(f'CALL tweet_insert_procedure({user_id}, "{tweet_text}")')
-        print(executable)
         conn.execute(executable)
+        conn.commit()
 
 
 tweet_samples.apply(insert_tweet, axis=1)
-
+with engine.connect() as conn:
+    tweet_testing = conn.execute(text("SELECT * FROM TWEET")).fetchall()
+tweet_testing = pd.DataFrame.from_dict(tweet_testing)
+print(tweet_testing)
